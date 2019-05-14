efd149d0961cd8ecd1a784c1205e38cb7898f5f1
Sean Hale <shale@tetrazoid.net>
2019-05-14 08:50:13 -0500

    Making it work

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ import png
 import sys
 import argparse
 from colour import Color
+import colorsys
 parser = argparse.ArgumentParser("generator")
 parser.add_argument("-c", "--hex", help="Hex Value", type=str, required=True)
 parser.add_argument("-o", "--output", help="Output file",
@@ -21,52 +22,65 @@ def get_value_for_png(color):
     return color
 
 
-def add_to_value(float_value, number):
-    return round((float_value * 255) + number) / 255
+def add_to_value(float_value, number, scale):
+    new_value = round((float_value * scale) + number)
+    if new_value < scale:
+        return new_value/scale
+    else:
+        return 1.0
 
 
-def subtract_from_value(float_value, number):
-    return round((float_value * 255) - number) / 255
+def subtract_from_value(float_value, number, scale):
+    new_value = round((float_value * scale) - number)
+    if new_value > 0:
+        return new_value/scale
+    else:
+        return 0.0
 
 
-first_hue = subtract_from_value(initial_color.hue, 80)
-first_saturation = subtract_from_value(initial_color.saturation, 55)
-first_luminance = subtract_from_value(initial_color.luminance, 55)
-first_color = Color(hsl=(first_hue, first_saturation, first_luminance))
+first_hsb_values = colorsys.rgb_to_hsv(
+    initial_color.get_red(), initial_color.get_green(), initial_color.get_blue())
+first_hue = subtract_from_value(first_hsb_values[0], 80, 360)
+first_saturation = subtract_from_value(first_hsb_values[1], 55, 100)
+first_brightness = subtract_from_value(first_hsb_values[2], 55, 100)
+first_color = Color(rgb=colorsys.hsv_to_rgb(
+    first_hue, first_brightness, first_saturation))
 
 all_colors = []
 all_colors.append(first_color)
 
-for i in range(1, 9):
+for i in range(0, 8):
     color = Color()
-    hue = add_to_value(first_color.hue, i*20)
+    last_hsb_values = colorsys.rgb_to_hsv(
+        all_colors[-1].get_red(), all_colors[-1].get_green(), all_colors[-1].get_blue())
+    hue = add_to_value(last_hsb_values[0], 20, 360)
     if(i <= 2):
-        saturation = add_to_value(all_colors[-1].saturation, 20)
-        luminance = add_to_value(all_colors[-1].luminance, 15)
+        saturation = add_to_value(last_hsb_values[1], 20, 100)
+        brightness = add_to_value(last_hsb_values[2], 15, 100)
     elif(i == 3):
-        saturation = add_to_value(all_colors[-1].saturation, 10)
-        luminance = add_to_value(all_colors[-1].luminance, 15)
+        saturation = add_to_value(last_hsb_values[1], 10, 100)
+        brightness = add_to_value(last_hsb_values[2], 15, 100)
     elif(i == 4):
-        saturation = add_to_value(all_colors[-1].saturation, 5)
-        luminance = add_to_value(all_colors[-1].luminance, 10)
+        saturation = add_to_value(last_hsb_values[1], 5, 100)
+        brightness = add_to_value(last_hsb_values[2], 10, 100)
     elif(i <= 6):
-        saturation = subtract_from_value(all_colors[-1].saturation, 15)
-        luminance = add_to_value(all_colors[-1].luminance, 10)
-    elif(i <= 8):
-        saturation = subtract_from_value(all_colors[-1].saturation, 15)
-        luminance = add_to_value(all_colors[-1].luminance, 5)
-    color = Color(hsl=(hue, saturation, luminance))
+        saturation = subtract_from_value(last_hsb_values[1], 15, 100)
+        brightness = add_to_value(last_hsb_values[2], 10, 100)
+    elif(i < 9):
+        saturation = subtract_from_value(last_hsb_values[1], 15, 100)
+        brightness = add_to_value(last_hsb_values[2], 5, 100)
+    color = Color(rgb=colorsys.hsv_to_rgb(hue, saturation, brightness))
     all_colors.append(color)
 
 color_width = round(255 / len(all_colors))
-color_height = round(255 / len(all_colors))
+color_height = 9
 pixels = []
 
 for color in all_colors:
     pixels.append(get_value_for_png(color) * color_width)
 
-rows = pixels * color_height
+rows = pixels
 png_writer = png.Writer(width=color_width,
-                        height=252, alpha='RGBA')
+                        height=color_height, alpha='RGBA')
 with open(args.output, 'wb') as img:
     png_writer.write(img, rows)
